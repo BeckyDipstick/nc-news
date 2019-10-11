a6ab77364df8efb6d09e50c09595f853bdb36cf8
Rebecca Collins <beckycols91@gmail.com>
2019-10-11 14:59:51 +0100

    added .all to comments router

--- routes/comments-router.js
+++ routes/comments-router.js
@@ -2,12 +2,14 @@ const {
 	patchCommentById,
 	deleteCommentById
 } = require('../controllers/comments-controllers');
+const { send405Error } = require('../error-handlers/handle405Errors');
 
 commentsRouter = require('express').Router();
 
 commentsRouter
 	.route('/:comment_id')
 	.patch(patchCommentById)
-	.delete(deleteCommentById);
+	.delete(deleteCommentById)
+	.all(send405Error);
 
 module.exports = { commentsRouter };
